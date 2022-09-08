abc3e47c28faeda0696fa14bb766f930f8f1a21e
nhancie <nancyonoh39@gmail.com>
2022-09-08 04:35:03 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* 
+ * Description: Print programming is like building a multilingual puzzle
+ * Return 0 if successful
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
